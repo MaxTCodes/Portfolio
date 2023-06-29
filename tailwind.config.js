a83218f8afb0bb58c10cab5d2b8152ea25b22143
Max T <maxt@maxt.church>
2023-06-29 10:23:19 -0400

    Changing build technologies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{jsx,tsx}",
+  ],
+  safelist: [
+    'decoration-auraside',
+    'decoration-mantle',
+    'decoration-goldfish',
+    'decoration-linkedin',
+    'decoration-github',
+    'decoration-sirius',
+    'decoration-email',
   ],
   theme: {
     extend: {
+      textDecorationColor: {
+        auraside: "#ed1c49",
+        mantle: "#fd3a4a",
+        goldfish: "#F6A01A",
+        linkedin: "#0A66C2",
+        github: "#777",
+        sirius: "#fff",
+        email: "#823AFD"
+      },
       colors: {
-        "primary": "#00050e"
+        primary: "#00050e",
       }
     },
   },
