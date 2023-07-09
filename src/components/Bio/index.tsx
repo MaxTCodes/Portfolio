8780762bd1208bd78e6ae861280fa0662d7c017d
Max T <maxt@maxt.church>
2023-07-08 21:15:17 -0400

    Updating linkedin

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -16,7 +16,7 @@ export const Bio = () => {
 				<p className="mt-4 text-gray-200 sm:text-sm tracking-tight">Hello! I&apos;m a senior in high school who has a strong passion for <Link color="github" href="https://github.com/maxtcodes">software engineering</Link>!</p>
 				<p className="mt-4 text-gray-200 sm:text-sm tracking-tight">I have been coding for six years. I&apos;ve worked at <Link color="auraside" href="https://auraside.com/">Auraside</Link>, there I made the current MacOS app for <Link color="mantle" href="https://Mantle.gg/">Mantle</Link>!</p>
 				<p className="mt-4 text-gray-200 sm:text-sm tracking-tight">I&apos;m currently working at <Link href="https://sirius.menu/">Sirius</Link> as a Software Engineer, and as a Deck Supervisor at <Link color="goldfish" href="https://www.goldfishswimschool.com/" >Goldfish Swim School</Link>!</p>
-				<p className="mt-4 text-gray-200 sm:text-sm tracking-tight">Feel free to contact me with any questions by connecting on <Link color="linkedin" href="https://www.linkedin.com/in/dominick-thakur-b60130217/">LinkedIn</Link> or <Link color="email" href="mailto:max@maxthakur.xyz">send me an email</Link>!</p>
+				<p className="mt-4 text-gray-200 sm:text-sm tracking-tight">Feel free to contact me with any questions by connecting on <Link color="linkedin" href="https://www.linkedin.com/in/dominick-thakur">LinkedIn</Link> or <Link color="email" href="mailto:max@maxthakur.xyz">send me an email</Link>!</p>
 			</div>
 		</>
 	)
